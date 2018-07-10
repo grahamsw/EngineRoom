8703aaff9310457058b6568138cae1d0b51d301f
grahamsw <grahams@pipeline.com>
2018-07-10 08:41:35 -0400

    framework implementation now receives event triggers by OSC from python

---
+++ Supercollider/CompositionFramework/send2framework.py
@@ -0,0 +1,14 @@
+
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+
+def sendSC(addr, val):
+    sendMsg('127.0.0.1', 57120, addr, val)
+    
+def sendMsg(ip, port, addr, val):
+  client = udp_client.SimpleUDPClient(ip, port)
+  client.send_message(addr, val)
+
+
