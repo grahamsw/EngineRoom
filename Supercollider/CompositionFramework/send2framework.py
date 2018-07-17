6a4749c3c2fa5960b009d5d046b0cd1c17b60070
Graham Stalker-Wilde <grahams@pipeline.com>
2018-07-17 11:29:40 -0400

    clean, working framework

--- Supercollider/CompositionFramework/send2framework.py
+++ Supercollider/CompositionFramework/send2framework.py
@@ -1,4 +1,5 @@
 from pythonosc import udp_client
+import time
 
 # can send an array 
 def sendSC(addr, val):
@@ -9,8 +10,28 @@ def sendMsg(ip, port, addr, val):
   client.send_message(addr, val)
 
 
-sendSC('/testImplOsc', ['makeS'])
-#sendSC('/testImplOsc', ['killS'])
-sendSC('/testImplOsc', ['setFreq', 300])
-sendSC('/testImplOsc', ['setAmp', 0.8])
+
+
+# sample using testImp.scd
+# in practise event messages will be much less frequent - fast and 'musical' scheduling
+# will be built in to the SuperCollider Events
+
+freq = 100
+mult = 1.1
+amp = 0.8
+ampMult = 0.98
+sleep = 0.05
+
+
+sendSC('/implOsc', ['makeS'])
+for i in range (40):
+    sendSC('/implOsc', ['setFreq', freq ])
+    freq = freq * mult
+    sendSC('/implOsc', ['setAmp', amp])
+    amp = amp * ampMult
+    time.sleep(sleep)
+    
+    
+sendSC('/implOsc', ['killS'])   
+    
 
