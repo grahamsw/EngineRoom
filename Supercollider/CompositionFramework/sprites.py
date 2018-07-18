a20543939c82a1cbc6bf45c0191fcaa3f8838898
grahamsw <grahams@pipeline.com>
2018-07-18 01:07:33 -0400

    python code for generate sprite random walks

--- Supercollider/CompositionFramework/sprites.py
+++ Supercollider/CompositionFramework/sprites.py
@@ -23,16 +23,42 @@ def nextRandom(mn, mx, cur, steps=10 ):
     
 
 
-def spriteValueGenerator(sprite):
-    pos = (sprite['posRange'][1]-sprite['posRange'][0])/2.0
-    freq = (sprite['freqRange'][1]-sprite['freqRange'][0])/2.0
-    amp = (sprite['ampRange'][1]-sprite['ampRange'][0])/2.0
-    rate = (sprite['rateRange'][1]-sprite['rateRange'][0])/2.0
+def spriteValueGenerator(sprite, steps = 10):
+    pos = sprite['posRange'][0] + (sprite['posRange'][1]-sprite['posRange'][0])/2.0
+    freq = sprite['freqRange'][0] + (sprite['freqRange'][1]-sprite['freqRange'][0])/2.0
+    amp = sprite['ampRange'][0] + (sprite['ampRange'][1]-sprite['ampRange'][0])/2.0
+    rate = sprite['rateRange'][0] + (sprite['rateRange'][1]-sprite['rateRange'][0])/2.0
+    def newVal(val):
+        if val == 'pos':
+            nonlocal pos
+            pos = nextRandom(sprite['posRange'][0], sprite['posRange'][1], pos, steps)
+            return pos
+        elif val == 'freq':
+            nonlocal freq
+            freq = nextRandom(sprite['freqRange'][0], sprite['freqRange'][1], freq, steps)
+            return freq
+        elif val == 'amp':
+            nonlocal amp
+            amp = nextRandom(sprite['ampRange'][0], sprite['ampRange'][1], amp, steps)
+            return amp
+        elif val == 'rate':
+            nonlocal rate
+            rate = nextRandom(sprite['rateRange'][0], sprite['rateRange'][1], rate, steps)
+            return rate
+        else:
+            print ('bad property')
+    return newVal
     
+    
+        
+
+
+generator = spriteValueGenerator(sprite1, 4)
+
+r = [generator('pos') for _ in range(10)]
 
 
 
 
-
-
+(sprite1['posRange'][1]-sprite1['posRange'][0])/2.0
 
