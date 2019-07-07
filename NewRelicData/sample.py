5733e0ac4e9e67a1a453595ff783b39a9acd35aa
grahamsw <grahams@pipeline.com>
2019-07-06 22:24:00 -0400

    random generator and proxy_gen, to allow generators to be swapped live

--- NewRelicData/sample.py
+++ NewRelicData/sample.py
@@ -3,18 +3,16 @@ os.chdir(r"C:\Users\graha\Documents\dev\EngineRoom\NewRelicData")
 
 
 from send2framework import sender
-from threadrunners import rlocker, run_in_thread, const_gen, rng_gen, zip_gen, seq_gen, AtEnd
+from threadrunners import rlocker, run_in_thread
+from generators import const_gen, rng_gen, rng_gen2, zip_gen, rand_gen, seq_gen, gen_proxy, AtEnd, \
+                                    makeSafeKeyedSetterGetter, keyed_gen
 
 
 
 # a threadsafe sender
 s = rlocker(sender('/implOsc'))
 
-
-s('loadCinitSynthode', r"C:\Users\graha\Documents\dev\EngineRoom\CompositionFramework\examples\sitemonitor\loadme.scd")        
-
-
-
+s('loadCode', r"C:\Users\graha\Documents\dev\EngineRoom\Archive\Supercollider\bell.scd")
 s('loadCode', r"C:\Users\graha\Documents\dev\EngineRoom\CompositionFramework\examples\sprites\whistle.scd")        
 
  #|freq = 440, focus = 4, rq = 0.03, pulseFreq = 5, amp = 0.5, out = 0|
@@ -28,20 +26,80 @@ s('playSynth', 'whistle',
                'rq', 0.03) 
 
 
-# create an instance of sonfier and set up some controls for it
-s('initSynth', 'sonifier', 'sonifier1', 'killsonifier1',
-               'freq', 'freq1', 400,
-               'amp', 'amp1', 0.1,
-               'rate', 'rate1', 0.4)
-
 params_zip = [const_gen(arg) for arg in ['playSynth', 'whistle', 'amp', 1, 'focus', 1, 'pulseFreq', 4, 'rq', 0.3, 'freq']]
 params_zip.append(seq_gen([300, 600, 900, 1200], AtEnd.MIRROR))
 params_zip.append(const_gen('timeScale'))
 params_zip.append(rng_gen(0.1, 4, 20, AtEnd.REPEAT))
 
-s1, _ = run_in_thread(s, zip_gen(*params_zip),
-                    const_gen(0.1))
+params = [const_gen('playSynth'),
+                 const_gen('whistle'),
+                 const_gen('amp'), const_gen(0.1),
+                 const_gen('focus'), rng_gen(0, 0.1, 10, AtEnd.MIRROR),
+                 const_gen('pulseFreq'), rng_gen(0.1, 5, 10, AtEnd.REPEAT),
+                 const_gen('rq'), const_gen(0.9),
+                 const_gen('timeScale'), seq_gen([0.5, 1, 2], AtEnd.REPEAT),
+                 const_gen('freq'), seq_gen([300, 450, 600, 750, 900, 1000], AtEnd.MIRROR)]
+
+s1, _ = run_in_thread(s, zip_gen(*params),
+                      const_gen(0.5))
 s1.set()
+
+
+
+
+s('loadCode', r"C:\Users\graha\Documents\dev\EngineRoom\Archive\Supercollider\bell.scd")
+
+
+
+
+
+#|fs=1000, t60=1, pitchy=1, amp=0.25, gate=1|
+freq_setter, freq_getter = makeSafeKeyedSetterGetter(rand_gen([300, 450, 600, 750, 900, 1000]))
+t60_setter, t60_getter = makeSafeKeyedSetterGetter(seq_gen([3,5,6], AtEnd.REPEAT))
+amp_setter, amp_getter = makeSafeKeyedSetterGetter(seq_gen([0.01, 0.02, 0.01, 0.01, 0.04], AtEnd.REPEAT))
+pitchy_setter, pitchy_getter = makeSafeKeyedSetterGetter(const_gen(3))
+
+dur_setter, dur_getter = makeSafeKeyedSetterGetter(rand_gen([0.1, 0.5, 1, 1.5,2]))
+
+
+# this is actually quite pretty
+params = [const_gen('playSynth'), const_gen('bell'),
+          const_gen('fs'),        gen_proxy(freq_getter),
+          const_gen('t60'),       gen_proxy(t60_getter),
+          const_gen('amp'),       gen_proxy(amp_getter),
+          const_gen('pitchy'),    gen_proxy(pitchy_getter)]
+
+s1, _ = run_in_thread(s, zip_gen(*params),
+                      gen_proxy(dur_getter))
+
+t60_setter(const_gen(4))
+t60_setter(seq_gen([3,5,6], AtEnd.MIRROR))
+
+dur_setter(rand_gen([0.1, 0.5, 1, 1.5,2], allowRepeats=False))
+freq_setter(rand_gen([300, 450, 600, 750], allowRepeats=False))
+
+dur_setter(rand_gen([0.5, 0.1, 1], allowRepeats=False))
+freq_setter(rand_gen([750, 900, 1000, 1150, 1270], allowRepeats=False))
+
+dur_setter(rand_gen([0.5, 0.1, 0.2, 0.05], allowRepeats=False))
+freq_setter(rand_gen([1000, 1150, 1270, 1500, 800], allowRepeats=False))
+
+s1.set()
+
+s('loadCode', r"C:\Users\graha\Documents\dev\EngineRoom\Archive\Supercollider\supercollider\PMCrotale.scd")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # now run some threads playing with the controls
 s1 = run_in_thread(s, zip_gen(const_gen("rate1"), rng_gen(0.1, 10, 20, 'rev')), 
@@ -79,15 +137,3 @@ s4.set()
 s5.set()
 s6.set()
 s('killsynth2')
-
-#
-#from ValueMappers import makeConstrainMapper
-#
-#freqMapper = makeConstrainMapper(1, 100, 100, 5000, logmap = True)
-#bwrMapper = makeConstrainMapper(1,100, 1, 0.1)
-#for i in np.linspace(0, 100, 100, False):
-#    time.sleep(0.01)
-#    val = freqMapper(i)
-#    print(i, val)
-#    s('freq2',  freqMapper(i))
-#    s('bwr2', bwrMapper(i))
--- NewRelicData/threadrunners.py
+++ NewRelicData/threadrunners.py
@@ -2,13 +2,8 @@ import threading
 import numpy as np
 import time
 import functools
-from enum import Enum
 
-class AtEnd(Enum):
-    STOP = 0
-    REPEAT = 1
-    MIRROR = 2
-    
+  
 
 def rlocker(func):
     rlock = threading.RLock()
@@ -52,46 +47,4 @@ def run_in_thread(fn, vals, durs, startImmediately=True):
     return s,t
 
 
-
-def const_gen(n):
-    while True:
-        yield n
-
-def rng_gen(fr, to, steps, atEnd =  AtEnd.STOP):
-    '''
-    atEnd is AtEnd.STOP to stop, 'AtEnd.Mirror' to reverse, and continue
-    forever, and AtEnd.REPEAT (or anything else, for now), to
-    repeat the forward sequence indefinitely
-    '''
-    rg = list(np.linspace(fr, to, steps))
-    if atEnd == AtEnd.MIRROR:
-        revg = rg.copy()[-2:0:-1]
-        rg.extend(revg)
-    l = len(rg)                
-    cur = 0    
-    while True:
-        if cur < l:
-            yield rg[cur]
-            cur += 1
-            if cur == l:
-                if atEnd == AtEnd.STOP:
-                    yield None
-                else:
-                    cur = 0
-def seq_gen(sq, atEnd=AtEnd.STOP):
-    cursor = rng_gen(0, len(sq)-1, len(sq), atEnd)
-    while True:
-        c = next(cursor)
-        if c == None:
-            yield None
-        else:
-            yield sq[int(c)]
-    
-
-        
-def zip_gen(*args):
-    while True:
-        l = []
-        for g in args:
-            l.append(next(g))
-        yield l
+            
